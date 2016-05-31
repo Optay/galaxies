5b549cd6ec873465459b7e0d058e935d3bf3174f
Robert Plante <robertdplante@gmail.com>
2016-05-31 19:32:40 -0400

    UFO laser bullet speed increased.

--- source/js/laserBullet.js
+++ source/js/laserBullet.js
@@ -27,7 +27,7 @@ galaxies.LaserBullet.prototype = {
 
         this.state = "inactive";
 
-        galaxies.fx.showLaserHit(this.object.position.clone().add(this.velocity.normalize().multiplyScalar(1.5)));
+        galaxies.fx.showLaserHit(this.object.position.clone().add(this.velocity.normalize().multiplyScalar(2.5)));
     },
 
     initModel: function () {
