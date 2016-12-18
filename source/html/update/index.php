5f5c310ce4c7d28a4aea414f4718c24c8d20811f
Robert Plante <robertdplante@gmail.com>
2016-12-17 22:52:15 -0600

    Making sure the php script doesn't run the build if the repository is up-to-date.

--- source/html/update/index.php
+++ source/html/update/index.php
@@ -1,6 +1,6 @@
 <?php
 putenv('PATH=/usr/local/bin:/bin:/usr/bin:/opt/aws/bin');
-exec('cd ../../; git pull && grunt build', $output, $returnCode);
+exec('cd ../../; if [ "$(git pull)" != "Already up-to-date." ]; then grunt build; fi', $output, $returnCode);
 
 if (empty($_POST)) {
   foreach ($output as $value) {
