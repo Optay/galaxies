a036bfd1140bdafda376ff54fa794b42004dd93a
Robert Plante <robertdplante@gmail.com>
2016-12-17 22:48:28 -0600

    Updating the php update script so that it doesn't return anything for post requests.

--- source/html/update/index.php
+++ source/html/update/index.php
@@ -2,6 +2,8 @@
 putenv('PATH=/usr/local/bin:/bin:/usr/bin:/opt/aws/bin');
 exec('cd ../../; git pull && grunt build', $output, $returnCode);
 
-foreach ($output as $value) {
-  echo $value . "\n";
+if (empty($_POST)) {
+  foreach ($output as $value) {
+    echo $value . "<br>\n";
+  }
 }
